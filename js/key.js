d0287c7cb955222b9cbc4d8c666e385a849ac1ef
Randy Miller <randy918@gmail.com>
2023-01-27 17:58:32 -0600

    basic setup

---
+++ js/key.js
@@ -0,0 +1,18 @@
+// const searchURLOne = "https://api.themoviedb.org/3";
+// let searchMajorCategoryTwo = "/search/movie";
+// const searchPrefixThree = "?api_key=";
+// const searchAPI_KEYFour = "ca1f26ba6eb2ba4fb641257b39d3bea7";
+// const searchQueryPrefixFive = "&query=";
+// let searchQuerySubjectSix = "furious";
+// const searchPagePrefixSeven = "&page="
+// let searchPageEight = 1
+
+// let searchAPI =
+//   searchURLOne +
+//   searchMajorCategoryTwo +
+//   searchPrefixThree +
+//   searchAPI_KEYFour +
+//   searchQueryPrefixFive +
+//   searchQuerySubjectSix + 
+//   searchPagePrefixSeven + 
+//   searchPageEight;
